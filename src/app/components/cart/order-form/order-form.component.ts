6ecd136d176eff443d15a0b6be29066c0c0f7328
米苏 <zhoux0130@hotmail.com>
2022-11-02 21:07:19 +0800

    fix: ngModelChange

--- src/app/components/cart/order-form/order-form.component.ts
+++ src/app/components/cart/order-form/order-form.component.ts
@@ -12,17 +12,29 @@ export class OrderFormComponent implements OnInit {
 
   name: string = '';
   address: string = '';
-  creditNo: number = 0;
+  creditNo: string = '';
+
+  errorFlag: boolean = false;
 
   constructor(private router: Router, private cartService: CartService) { }
   ngOnInit(): void {
+  }
+
+  validateCardNo(arg:string): void{
+    console.log(/^\d{16}$/.test(arg))
+    // 正则表达式判断，字符串是不是正好16个字符，并且都是数字
+    if(!/^\d{16}$/.test(arg)){
+      this.errorFlag = true;
+    }else{
+      this.errorFlag = false;
+    }
   }
 
   submitForm(): void{
     const contact: Contact = {
       name: this.name,
       address: this.address,
-      creditCard: this.creditNo
+      creditCard: parseInt(this.creditNo)
     }
   
     this.cartService.ordered(contact);
